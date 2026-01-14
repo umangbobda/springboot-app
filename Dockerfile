adb213c0e560c234e1c890d11212610c4d0a6e62
umangbobda <umangbobda2@gmail.com>
2026-01-14 08:24:51 -0800

    dockerizing the app

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM eclipse-temurin:17-jre
+
+WORKDIR /app
+
+COPY target/demoProject-0.0.1-SNAPSHOT.jar app.jar
+COPY Player.csv Player.csv
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/app/playerservicejava/service/chat/ChatClientService.java
+++ src/main/java/com/app/playerservicejava/service/chat/ChatClientService.java
@@ -21,11 +21,21 @@ import java.util.List;
 public class ChatClientService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ChatClientService.class);
     private static final String MODEL = OllamaModelType.TINYLLAMA;
+    private static String baseURL="baseURL";
 
-    @Autowired
-    private OllamaAPI ollamaAPI;
+    private final OllamaAPI ollamaAPI;
+
+    public ChatClientService() {
+        baseURL = System.getenv().getOrDefault(
+                "OLLAMA_BASE_URL",
+                "http://localhost:11434"
+        );
+        LOGGER.info("Base URL ========= {}", baseURL);
+        this.ollamaAPI = new OllamaAPI(baseURL);
+    }
 
     public List<Model> listModels() throws OllamaBaseException, IOException, URISyntaxException, InterruptedException {
+        LOGGER.error("Base URL ========= {}", baseURL);
         List<Model> models = ollamaAPI.listModels();
         return models;
     }
